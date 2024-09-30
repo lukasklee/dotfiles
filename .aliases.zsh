e89db0df896e9ee2e0fe2b73cf84c0f0f297fea5
Lukas Klee <code@lukasklee.de>
2024-09-30 08:53:17 +0200

    Remove spotify_player alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,7 +1,6 @@
 # Tools
 alias v="nvim"
 alias lg="lazygit"
-alias sf="spotify_player"
 alias ls="eza -l -a --icons --group-directories-first --ignore-glob=\"*.DS_Store*\""
 alias day="sh ~/.dotfiles/scripts/day"
 
